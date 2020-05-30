b8b30cc3244b6646c51637dbbb7ee7eac64baaa2
wonglok <yellowhappy831@gmail.com>
2020-05-30 09:18:31 +0800

    git backup

--- components/GLUnits/Content/CTQuoteDisplay.vue
+++ components/GLUnits/Content/CTQuoteDisplay.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="search" class="m-3 border border-gray-500 text-2xl rounded-full px-4 focus:outline-none" placeholder="Filter Quotes" v-model="query">
+    <input type="search" class="m-3 border border-gray-500 text-2xl rounded-full px-4 focus:outline-none" :placeholder="`Filter ${quotes.length} Quotes`" v-model="query">
     <div class="m-3 mb-5 border-gray-400 border shadow-lg rounded-lg bg-white" v-for="quote in quotes.filter(filterQuotes)" :key="quote._id">
       <div class="p-3">{{ quote.sentence }}</div>
       <div class="py-2 px-3 text-right border-t border-gray-400 text-xs text-gray-600" v-if="quote.author || quote.overrideAutor">
